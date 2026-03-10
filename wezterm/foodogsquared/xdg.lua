4f297a690bbcce4510cb38da6c2a1c3ff31c0354
Gabriel Arazas <foodogsquared@foodogsquared.one>
2026-03-10 22:54:26 +0800

    wezterm/xdg: consider XDG base directories for `xdg_user_dir`
    Make it a convenience function at this point.

--- wezterm/foodogsquared/xdg.lua
+++ wezterm/foodogsquared/xdg.lua
@@ -7,19 +7,31 @@ local M = {}
 local fds_str = require("foodogsquared/utils/strings")
 local wezterm = require("wezterm")
 
----Given a name of a XDG user directory, return its value. If there's no such
----thing, it will always return the home directory.
+--- Given a name of a XDG user directory including the user base directories
+--- (e.g., XDG_DATA_HOME, XDG_CACHE_HOME), return its value. If there's no such
+--- value for the XDG user directory, it will always default to the home
+--- directory.
 ---@param n string
 ---@return string
 function M.xdg_user_dir(n)
-  return os.getenv("XDG_" .. n .. "_DIR") or wezterm.home_dir
+  if n == "XDG_CONFIG_HOME" then
+    return os.getenv("XDG_CONFIG_HOME") or (wezterm.home_dir .. "/.config")
+  elseif n == "XDG_DATA_HOME" then
+    return os.getenv("XDG_DATA_HOME") or (wezterm.home_dir .. "/.local/share")
+  elseif n == "XDG_CACHE_HOME" then
+    return os.getenv("XDG_CACHE_HOME") or (wezterm.home_dir .. "/.cache")
+  elseif n == "XDG_STATE_HOME" then
+    return os.getenv("XDG_STATE_HOME") or (wezterm.home_dir .. "/.local/state")
+  else
+    return os.getenv("XDG_" .. n .. "_DIR") or wezterm.home_dir
+  end
 end
 
 ---Parse the XDG user dirs file normally found at `$XDG_CONFIG_HOME/user-dirs.dirs`.
 ---@param path string?
 ---@return table
 function M.parse_xdg_user_dirs(path)
-  local dirsfile = path or ((os.getenv("XDG_CONFIG_HOME") or (wezterm.home_dir .. "/.config")) .. "/user-dirs.dirs")
+  local dirsfile = path or (M.xdg_user_dir("XDG_CONFIG_HOME") .. "/user-dirs.dirs")
   local res = {}
 
   io.input(dirsfile)
@@ -27,15 +39,15 @@ function M.parse_xdg_user_dirs(path)
   local d = io.read("l")
   while d ~= nil do
     if
-      fds_str.starts_with(d, "#")
-      or fds_str.starts_with(d, " ")
-      or fds_str.starts_with(d, "\t")
-      or not (fds_str.starts_with(d, "XDG_") or fds_str.ends_with(d, "_DIR"))
+      d:starts_with("#")
+      or d:starts_with(" ")
+      or d:starts_with("\t")
+      or (d:starts_with("XDG_") and d:ends_with("_DIR"))
     then
       goto continue
     else
       ---@diagnostic disable-next-line: missing-parameter
-      local data = string.split(d, "=", 1)
+      local data = d:split("=", 1)
       local basedir = data[1]:gsub("^XDG_", ""):gsub("_DIR$", "")
       res[basedir] = data[2]:gsub('^%s*"', ""):gsub('"', ""):gsub("%$(%w+)", os.getenv)
     end
